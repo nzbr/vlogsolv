fb8e5d8e64359c4b327ba5ac43933eea81e6797c
nzbr <mail@nzbr.de>
2019-10-13 02:12:46 +0200

    Print character instead of byte value on panic for malformed expression

--- evaluate.v
+++ evaluate.v
@@ -6,41 +6,41 @@ fn evaluate_expression(expr string) Expression {
 		c := expr[i]
 		match c {
 			`&` => {
-				if symstack.len < 2 { panic('Malformed expression at $i -> $c') }
+				if symstack.len < 2 { panic('Malformed expression at $i -> ${c.str()}') }
 				expa := symstack[symstack.len - 1]
 				expb := symstack[symstack.len - 2]
 				symstack = symstack.slice(0, symstack.len - 2)
 				symstack << Expression { exps: [expa, expb], eval: eval_and }
 			}
 			`|` => {
-				if symstack.len < 2 { panic('Malformed expression at $i -> $c') }
+				if symstack.len < 2 { panic('Malformed expression at $i -> ${c.str()}') }
 				expa := symstack[symstack.len - 1]
 				expb := symstack[symstack.len - 2]
 				symstack = symstack.slice(0, symstack.len - 2)
 				symstack << Expression { exps: [expa, expb], eval: eval_or }
 			}
 			`!` => {
-				if symstack.len < 1 { panic('Malformed expression at $i -> $c') }
+				if symstack.len < 1 { panic('Malformed expression at $i -> ${c.str()}') }
 				exp := symstack[symstack.len - 1]
 				symstack = symstack.slice(0, symstack.len - 1)
 				symstack << Expression { exps: [exp], eval: eval_not }
 			}
 			`=` => {
-				if symstack.len < 2 { panic('Malformed expression at $i -> $c') }
+				if symstack.len < 2 { panic('Malformed expression at $i -> ${c.str()}') }
 				expa := symstack[symstack.len - 1]
 				expb := symstack[symstack.len - 2]
 				symstack = symstack.slice(0, symstack.len - 2)
 				symstack << Expression { exps: [expa, expb], eval: eval_eq }
 			}
 			`>` => {
-				if symstack.len < 2 { panic('Malformed expression at $i -> $c') }
+				if symstack.len < 2 { panic('Malformed expression at $i -> ${c.str()}') }
 				expa := symstack[symstack.len - 1]
 				expb := symstack[symstack.len - 2]
 				symstack = symstack.slice(0, symstack.len - 2)
 				symstack << Expression { exps: [expa, expb], eval: eval_impl }
 			}
 			`^` => {
-				if symstack.len < 2 { panic('Malformed expression at $i -> $c') }
+				if symstack.len < 2 { panic('Malformed expression at $i -> ${c.str()}') }
 				expa := symstack[symstack.len - 1]
 				expb := symstack[symstack.len - 2]
 				symstack = symstack.slice(0, symstack.len - 2)
